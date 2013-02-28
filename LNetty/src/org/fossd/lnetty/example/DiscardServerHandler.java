b3ef3a7dae9269b710a7725fbac7e862bc5c7523
Talin <Talin@Talin-PC>
2013-02-28 19:18:57 +0800

    try write back

--- LNetty/src/org/fossd/lnetty/example/DiscardServerHandler.java
+++ LNetty/src/org/fossd/lnetty/example/DiscardServerHandler.java
@@ -1,7 +1,12 @@
 package org.fossd.lnetty.example;
 
 import org.jboss.netty.buffer.ChannelBuffer;
+import org.jboss.netty.buffer.ChannelBuffers;
+import org.jboss.netty.channel.Channel;
+import org.jboss.netty.channel.ChannelFuture;
+import org.jboss.netty.channel.ChannelFutureListener;
 import org.jboss.netty.channel.ChannelHandlerContext;
+import org.jboss.netty.channel.ChannelStateEvent;
 import org.jboss.netty.channel.ExceptionEvent;
 import org.jboss.netty.channel.MessageEvent;
 import org.jboss.netty.channel.SimpleChannelHandler;
@@ -9,17 +14,37 @@ import org.jboss.netty.channel.SimpleChannelHandler;
 public class DiscardServerHandler extends SimpleChannelHandler {
 
 	@Override
+	public void channelConnected(ChannelHandlerContext ctx, ChannelStateEvent e) throws Exception {
+		Channel ch = e.getChannel();
+		ChannelBuffer time = ChannelBuffers.buffer(4);
+		time.writeInt((int) (System.currentTimeMillis() / 1000L + 2208988800L));
+
+		ChannelFuture f = ch.write(time);
+
+		f.addListener(new ChannelFutureListener() {
+			public void operationComplete(ChannelFuture future) {
+				Channel ch = future.getChannel();
+				ch.close();
+			}
+		});
+	}
+
+	@Override
 	public void exceptionCaught(ChannelHandlerContext ctx, ExceptionEvent e) throws Exception {
-		super.exceptionCaught(ctx, e);
+		 e.getCause().printStackTrace();
+         e.getChannel().close();
 	}
 
 	@Override
 	public void messageReceived(ChannelHandlerContext ctx, MessageEvent e) throws Exception {
-		ChannelBuffer buf = (ChannelBuffer) e.getMessage();
-		while (buf.readable()) {
-			System.out.println((char) buf.readByte());
-			System.out.flush();
-		}
+		// ChannelBuffer buf = (ChannelBuffer) e.getMessage();
+		// while (buf.readable()) {
+		// System.out.println((char) buf.readByte());
+		// System.out.flush();
+		// }
+
+		// Channel ch = e.getChannel();
+		// ch.write(e.getMessage());
 	}
 
 }
